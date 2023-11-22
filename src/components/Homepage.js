36c1f3087484f85d3dbef7a31eb36731cfe927b5
KennedyShiku <kenrichshiku@gmail.com>
2023-11-22 22:40:54 +0300

    Added CoinList and Blog to Homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import '../App.css';
-
+import CoinList from './CoinList';
+import Blog from './Blog';
 
 
 const HomePage = () => {
 
   return (
-    <div className="intro">
-      <div className="intro-title">
-        <h1>Trade with us</h1>
-        <h2>Buy and sell cryptocurrency</h2>
-        <p>Fast and secure way to buy and<br/> exchange the top crypto coins</p>
-        <div className="intro-buttons">
-          <button className="get-started">Get Started</button>
-          <button className="browse-coins">Browse Now</button>
+    <div>
+      <div className="intro">
+        <div className="intro-title">
+          <h1>Trade with us</h1>
+          <h2>Buy and sell cryptocurrency</h2>
+          <p>Fast and secure way to buy and<br/> exchange the top crypto coins</p>
+          <div className="intro-buttons">
+            <button className="get-started">Get Started</button>
+            <button className="browse-coins">Browse Now</button>
+          </div>
+        </div>
+        <div className="intro-image">
+          <img src="https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=029" />       
         </div>
       </div>
-      <div className="intro-image">
-        <img src="https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=029" />       
-      </div>
-      <div>
-        
-      </div>
+      <CoinList />
+      <Blog />
     </div>
   );
 }
