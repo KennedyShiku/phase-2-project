223f337f625b9f1e3474b6f07078ef966291d94e
Spy <Spyngunga@gmail.com>
2023-11-23 00:28:34 +0300

    ignore

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import HomePage from './components/Homepage';
 import CoinList from './components/CoinList';
 import Blog from './components/Blog';
-
+import NavBar from './components/NavBar';
+import "./NavBar.css";
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <HomePage />
       <CoinList />
       <Blog />
