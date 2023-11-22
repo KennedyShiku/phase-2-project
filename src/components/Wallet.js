184604ea3fc462a740f3f4a379801384c9d7637c
ian <ianokipla@gmail.com>
2023-11-22 13:04:18 +0300

    added wallet.js files

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -9,6 +9,12 @@ function CryptoWallet({name, amount, imagesrc}){
                 <h1>{name}</h1>
                 <h2>Amount: {amount}</h2>
             </div>
+            <div className="post">
+             <h3>latest Post</h3> 
+             <p>Title: My Crypto Update</p>             
+             <p>Posted on: {new Date().toLocaleString()}</p>
+            </div>
+
         </React.Fragment>
     );
 }
